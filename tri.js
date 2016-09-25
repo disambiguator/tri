0cc19ba8d5ccadbf62b2600a4143cbd67a54816d
Paras <sanghaviparas@gmail.com>
2016-09-25 13:18:05 -0400

    Randomize

--- tri.js
+++ tri.js
@@ -8,13 +8,16 @@ var svgContainer = d3.select("body").append("svg")
 
 var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e'];
 
-$('body').click(function (e) {
+setInterval(function () {
+    var randomX = Math.floor(Math.random() * canvasWidth);
+    var randomY = Math.floor(Math.random() * canvasHeight);
+
     for (var i = 20; i > 0; i--) {
         svgContainer.append("circle")
             .attr("fill", colors[Math.floor(Math.random() * colors.length)])
-            .attr("cx", e.pageX)
-            .attr("cy", e.pageY)
+            .attr("cx", randomX)
+            .attr("cy", randomY)
             .attr("r", 0)
 	    .transition().attr("r", 5*i);
     }
-});
+}, 100);
