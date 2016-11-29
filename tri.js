3d9b7f27927d0f56be8b44a569892b56d81bd811
Paras <sanghaviparas@gmail.com>
2016-11-29 17:37:02 -0500

    Click to generate random targets

--- tri.js
+++ tri.js
@@ -1,16 +1,42 @@
-var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e'];
+const colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e']
+
+const max_size = 200
+
+var targets = []
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
+    createCanvas(windowWidth, windowHeight)
+    noStroke()
+    frameRate(15)
 }
 
 function draw() {
-    var randomX = Math.floor(Math.random() * windowWidth);
-    var randomY = Math.floor(Math.random() * windowHeight);
+    if (_.isEmpty(targets)) { return }
+   
+    _.each(targets, drawCircles) 
+    
+    targets = _.filter(targets, function(coords) {
+        return coords.numCircles < max_size
+    })
+}
 
-    for (var i = 20; i > 0; i--) {
-       fill(colors[Math.floor(Math.random() * colors.length)]);
-       ellipse(randomX, randomY, 5*i);
+function drawCircles(coords) {
+    for (var i = coords.numCircles; i > 0; i--) {
+        fill(randomColor())
+        ellipse(coords.x, coords.y, 5*i)
     }
+    
+    coords.numCircles++
+}
+
+function mousePressed() {
+    targets.push({
+        x: mouseX,
+        y: mouseY,
+        numCircles: 0
+    })
+}
+
+function randomColor() {
+    return _.sample(colors)
 }
