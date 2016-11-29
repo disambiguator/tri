97e9a3dae42552ed34d8da1d40a58acff7046a1e
Paras <sanghaviparas@gmail.com>
2016-11-29 14:24:02 -0500

    Add visualizer code

---
+++ visualizer.js
@@ -0,0 +1,28 @@
+var mic, fft, mysound;
+
+function preload() {
+  mySound = loadSound('https://dl.dropboxusercontent.com/u/265913/quantic.mp3');
+}
+
+function setup() {
+   mySound.setVolume(0.1);
+   mySound.play();
+
+   createCanvas(710,400);
+   noFill();
+
+   fft = new p5.FFT();
+   fft.setInput(mySound);
+}
+
+function draw() {
+   background(200);
+
+   var spectrum = fft.analyze();
+
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0) );
+   }
+   endShape();
+}
