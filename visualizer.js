d3d680bcde09acc3377585a954f103b20967083a
Paras <sanghaviparas@gmail.com>
2016-11-29 16:23:25 -0500

    Add eslinting

--- visualizer.js
+++ visualizer.js
@@ -1,28 +1,28 @@
-var mic, fft, mysound;
+var fft, mySound
 
 function preload() {
-  mySound = loadSound('https://dl.dropboxusercontent.com/u/265913/quantic.mp3');
+    mySound = loadSound('https://dl.dropboxusercontent.com/u/265913/quantic.mp3')
 }
 
 function setup() {
-   mySound.setVolume(0.1);
-   mySound.play();
+    mySound.setVolume(0.1)
+    mySound.play()
 
-   createCanvas(710,400);
-   noFill();
+    createCanvas(710,400)
+    noFill()
 
-   fft = new p5.FFT();
-   fft.setInput(mySound);
+    fft = new p5.FFT()
+    fft.setInput(mySound)
 }
 
 function draw() {
-   background(200);
+    background(200)
 
-   var spectrum = fft.analyze();
+    var spectrum = fft.analyze()
 
-   beginShape();
-   for (i = 0; i<spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0) );
-   }
-   endShape();
+    beginShape()
+    for (var i = 0; i<spectrum.length; i++) {
+        vertex(i, map(spectrum[i], 0, 255, height, 0))
+    }
+    endShape()
 }
