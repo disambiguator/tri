0984bdd0e3f8a34a7e43003e2f6fe26e1ffa6a04
Paras <sanghaviparas@gmail.com>
2016-11-29 00:00:35 -0500

    Switch from using d3js to p5js

--- tri.js
+++ tri.js
@@ -1,23 +1,16 @@
-var canvasWidth = $(window).width();
-var canvasHeight = $(window).height();
-
-//Make an SVG Container
-var svgContainer = d3.select("body").append("svg")
-    .attr("width", canvasWidth)
-    .attr("height", canvasHeight);
-
 var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e'];
 
-setInterval(function () {
-    var randomX = Math.floor(Math.random() * canvasWidth);
-    var randomY = Math.floor(Math.random() * canvasHeight);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+}
+
+function draw() {
+    var randomX = Math.floor(Math.random() * windowWidth);
+    var randomY = Math.floor(Math.random() * windowHeight);
 
     for (var i = 20; i > 0; i--) {
-        svgContainer.append("circle")
-            .attr("fill", colors[Math.floor(Math.random() * colors.length)])
-            .attr("cx", randomX)
-            .attr("cy", randomY)
-            .attr("r", 0)
-	    .transition().attr("r", 5*i);
+       fill(colors[Math.floor(Math.random() * colors.length)]);
+       ellipse(randomX, randomY, 5*i);
     }
-}, 100);
+}
